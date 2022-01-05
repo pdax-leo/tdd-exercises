6297197b95b926ffba874f7c8010b1b1c6bc0988
Leo Claudio <johnleo.claudio@pdax.ph>
2022-01-05 15:51:16 +0800

    update sum_tail test

--- calc/sum_tail_test.go
+++ calc/sum_tail_test.go
@@ -7,25 +7,29 @@ import (
 
 func TestSumTail(t *testing.T) {
 
-	checkSums := func(t testing.TB, got, want []int) {
-		t.Helper()
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+	cases := []struct {
+		Description string
+		Got         []int
+		Want        []int
+	}{
+		{
+			"given slice of number slices, returns a slice with the slice tail total",
+			SumTail([]int{1, 2, 3}, []int{1, 2}),
+			[]int{5, 2},
+		},
+		{
+			"safely sum empty slices",
+			SumTail([]int{}, []int{3, 4, 5}),
+			[]int{0, 9},
+		},
 	}
 
-	t.Run("given slice of number slices, returns a slice with the slice tail total", func(t *testing.T) {
-		numbers1 := []int{1, 2, 3}
-		numbers2 := []int{1, 2}
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			if !reflect.DeepEqual(test.Got, test.Want) {
+				t.Errorf("got %v want %v", test.Got, test.Want)
+			}
+		})
+	}
 
-		got := SumTail(numbers1, numbers2)
-		want := []int{5, 2}
-		checkSums(t, got, want)
-	})
-
-	t.Run("safely sum empty slices", func(t *testing.T) {
-		got := SumTail([]int{}, []int{3, 4, 5})
-		want := []int{0, 9}
-		checkSums(t, got, want)
-	})
 }
