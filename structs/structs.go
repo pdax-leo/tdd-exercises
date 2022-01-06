1a7c78982325d75851187b0bc0d12304f799094c
Leo Claudio <johnleo.claudio@pdax.ph>
2022-01-06 21:46:59 +0800

    fix lint errors

--- structs/structs.go
+++ structs/structs.go
@@ -2,35 +2,43 @@ package structs
 
 import "math"
 
+// Shape struct
 type Shape interface {
 	Area() float64
 }
 
+// Rectangle struct
 type Rectangle struct {
 	width  float64
 	height float64
 }
 
+// Area func
 func (r Rectangle) Area() float64 {
 	return r.height * r.width
 }
 
+// Perimeter func
 func (r Rectangle) Perimeter() float64 {
 	return 2 * (r.height + r.width)
 }
 
+// Circle struct
 type Circle struct {
 	radius float64
 }
 
+// Area func
 func (c Circle) Area() float64 {
 	return math.Pi * c.radius * c.radius
 }
 
+// Smeagol struct
 type Smeagol struct {
 	eyeSize float64
 }
 
+// Area func
 func (s Smeagol) Area() float64 {
 	return s.eyeSize
 }
