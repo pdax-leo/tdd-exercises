96bf922f359d058b0ed41d9cb3f4310a69052884
Leo Claudio <johnleo.claudio@pdax.ph>
2022-01-11 22:05:39 +0800

    add select

---
+++ selectpack/select_test.go
@@ -0,0 +1,47 @@
+package selectpack
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	t.Run("returns the faster url", func(t *testing.T) {
+		slowServer := makeDelayedServer(20)
+		fastServer := makeDelayedServer(0)
+
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+
+		want := fastURL
+		got, _ := Racer(slowURL, fastURL)
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("returns an error if a server doesn't respond within the specified time", func(t *testing.T) {
+		server := makeDelayedServer(25 * time.Millisecond)
+
+		defer server.Close()
+
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
+
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
+	})
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
